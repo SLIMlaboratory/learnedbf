74195ebdb10c9e1683a9db87c360dd8c3293dbed
Dario Malchiodi <malchiodi@malchiodi.docenti.di.unimi.it>
2026-03-06 18:24:21 +0100

    deleted unused function run_BF

--- learnedbf/BF/__init__.py
+++ learnedbf/BF/__init__.py
@@ -520,34 +520,3 @@ class ClassicalBloomFilter(BloomFilter, BaseEstimator, ClassifierMixin):
 
         return self.bf_.get_size()
 
-
-def run_BF(FPR, key_set, testing_list):
-    '''
-    Crea un BF con key-set = key_set e target fpr = FPR
-    e ritorna FPR empirico, dimensione del BF e tempo di accesso medio per
-    elemento calcolati testando la struttura su testing_list.
-    '''
-
-    BF = BloomFilter(len(key_set), FPR)
-    for url in key_set:
-        BF.add(url)
-
-    fps = 0
-    total = 0
-    total_time = 0
-
-    for urlt in testing_list:
-        total += 1
-        start = time.time()
-        result = BF.check(urlt)
-        end = time.time()
-        total_time += (end-start)
-        if result:
-            fps += 1
-
-    avg_fp = fps/total
-    # print(f"avg fp : {fps/total} , fps :{fps}, total: {total},
-    #         {BF.check(testing_list[2])}")
-
-    # returns empirical FPR, BF size in bytes, and access time per element
-    return avg_fp, BF.size/8, (total_time)/len(testing_list)
